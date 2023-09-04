1aa35e43d1bb8e3c51b5933b4fc0bde3ec6992f6
palak jaiswal <palakjaiswalch28@gmail.com>
2023-09-04 22:58:25 +0530

    ui  footer

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -1,13 +1,36 @@
 import React from "react";
+import styles from './footer.module.css';
+import {BsTwitter} from 'react-icons/bs';
+import {MdOutlineEmail} from 'react-icons/md';
+import {BsInstagram} from 'react-icons/bs';
+import {BsLinkedin} from 'react-icons/bs';
+import {FaFacebookSquare} from 'react-icons/fa';
+import {RiCopyrightFill} from 'react-icons/ri';
 export default function Footer(){
     return<>
-        <footer className="bg-light-blue text-blue-700  font-bold py-6 h-50 w-100">
-        <div className="container mx-auto flex items-center justify-between">
-          <p className="p-10">{new Date().getFullYear()} Crack SSB.</p>
-          <div className=" p-10">
-            <p className="p-10 "> All rights reserved.</p>
-          </div>
-        </div>
-      </footer>
+          <footer>
+              <div className={styles.parent}>
+                <div className={styles.footerChild}>
+                 <h2 className={styles.linkHeader}>Social Links</h2>
+                 <div className={styles.links}>
+        <h2 className={styles.icons}><a href="  https://www.instagram.com/palakjaiswal1041/"><BsInstagram/></a></h2>
+        <h2 className={styles.icons}> <a href="https://www.linkedin.com/in/palak-jaiswal-121557254/"><BsLinkedin/></a></h2>
+        <h2 className={styles.icons}> <a href="https://twitter.com/palakja81325436"><BsTwitter/></a></h2>
+        <h2 className={styles.icons}><a href="http://www.facebook.com"></a><FaFacebookSquare/></h2>
+        <h2 className={styles.icons}><a href="mailto:palakjaiswalch28@gmail.com"><MdOutlineEmail/></a></h2>
+                 </div>
+                </div>
+                <div className={styles.footerChild}>
+                  <div className={styles.rights}>
+                    <div className={styles.right}> 
+                    <RiCopyrightFill className={styles.icons}/>
+                    <h1>{new Date().getFullYear()}</h1>
+                    <h1>copyright</h1>
+                    </div>
+                    <h1>Made by @palak Jaiswal</h1>         
+                  </div>
+                </div>
+              </div>          
+          </footer>
     </>
 }
